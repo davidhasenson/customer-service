37ab11950ebdd972ef94d4cb2550812c62f8a0e2
Daniel <daniel.dahl@yh.nackademin.se>
2026-08-31 10:41:18 -0300

    Fixed boot error with restTemplate and pom.xml

--- src/main/java/org/example/customerservice/customer/service/CustomerService.java
+++ src/main/java/org/example/customerservice/customer/service/CustomerService.java
@@ -15,7 +15,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
